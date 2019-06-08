7c5f39a112e799fe6f94f6ddb262fd53e4260a18
k-rutkowski <krzysztof.rutk@gmail.com>
2019-06-08 13:17:56 +0200

    Add bash config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 source="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$source" )" && pwd )"
+  source="$(readlink "$source")"
+  [[ $source != /* ]] && source="$DIR/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+dir="$( cd -P "$( dirname "$source" )" && pwd )"
 backup_dir=~/dotfiles-bckp
-files="vimrc vim"
+files="vimrc vim bash_aliases bash_extra"
+
+#echo "source: $source"
+#echo "dir: $dir"
+#echo "backup dir: $backup_dir"
+#echo "files: $files"
+#exit 0
 
 ## backup existing dotfiles
 mkdir -p "$backup_dir"
