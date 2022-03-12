e4b0ef1c0b1e123f2d90c012931e2eb234a4c6c9
Krzysztof Rutkowski <krzysztof.rutk@gmail.com>
2022-03-12 16:33:14 +0000

    Fix powerline configuration

--- install-packages.sh
+++ install-packages.sh
@@ -2,4 +2,5 @@
 
 sudo apt update -y
 sudo apt dist-upgrade -y
-sudo apt install -y python3 git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib neovim tmux golang golang-doc
+sudo apt install -y python3 python3-pip git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib neovim tmux golang golang-doc
+pip install powerline-shell
