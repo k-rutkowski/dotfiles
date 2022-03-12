05881324b0f1f3fc6c958400433be6fdb1f9b90e
Krzysztof Rutkowski <krzysztof.rutk@gmail.com>
2022-03-12 20:43:01 +0100

    Update script install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -3,4 +3,4 @@
 sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt install -y python3 python3-pip git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib neovim tmux golang golang-doc
-pip install powerline-shell
+pip install --user powerline-shell
