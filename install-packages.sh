bb77267b89e27a6e6ccbf8d9570a4a61f6c7a638
Krzysztof Rutkowski <krzysztof.rutk@gmail.com>
2022-04-17 01:16:58 +0200

    Update bash config and package installer script

--- install-packages.sh
+++ install-packages.sh
@@ -4,3 +4,5 @@ sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt install -y python3 python3-pip git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib neovim tmux golang golang-doc fonts-powerline
 pip install --user powerline-shell
+
+sudo apt install ttf-mscorefonts-installer
