17d85c7fb633c7b2012c2c03af28b80fb1df49d5
Krzysztof Rutkowski <krzysztof.rutk@gmail.com>
2022-10-02 18:22:31 +0000

    wip7

--- install-packages.sh
+++ install-packages.sh
@@ -18,7 +18,7 @@ $sudox apt dist-upgrade -y
 
 ## basic stuff
 echo "Installing core dependencies..."
-$sudox apt install -y curl python3 python3-pip git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib tmux fonts-powerline 
+$sudox apt install -y curl python3 python3-pip git git-doc git-lfs git-man clang-tools clang-tidy clang-format g++ g++-multilib tmux fonts-powerline nodejs npm
 #$sudox apt install -y ttf-mscorefonts-installer
 
 ## some cool tools
@@ -33,27 +33,27 @@ git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 $HOME/.fzf/install --all
 
 ## better cd
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+bash <(curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh)
 
 ## starship prompt
-curl -sS https://starship.rs/install.sh > /tmp/starship-install.sh
-sh /tmp/starship-install.sh -y
-rm -f /tmp/starship-install.sh
-
-## neovim 7.2
-echo "Installing neovim..."
-curl -L -O https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-$sudox apt install ./nvim-linux64.deb
-rm ./nvim-linux64.deb
+sh <(curl -sS https://starship.rs/install.sh) -y
 
 ## newest rust
 echo "Installing rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
-sh /tmp/rustup-init -y
-rm -f /tmp/rustup-init
-mkdir -p $HOME/.local/share/bash-completion/completions
+sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y
 source "$HOME/.cargo/env"
+mkdir -p $HOME/.local/share/bash-completion/completions
 rustup completions bash > $HOME/.local/share/bash-completion/completions/rustup
+
+## neovim 7.2
+echo "Installing neovim..."
+curl -sS -L -O https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
+$sudox apt install ./nvim-linux64.deb
+rm ./nvim-linux64.deb
+
+## neovim config template
+echo "Installing LunarVim..."
+bash <(curl -sS https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y --no-install-dependencies
 
 echo
 echo "-------------------------------------------------"
