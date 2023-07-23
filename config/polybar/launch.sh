512958f8e26e5249b879e2d535dbf639508d9195
Krzysztof <kr@pop-os.localdomain>
2023-07-23 10:12:23 +0200

    Refine i3 setup

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,9 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log #/tmp/polybar2.log
-polybar --config="~/.config/polybar/config.ini" bar 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+echo "---" | tee -a /tmp/polybar.log  >/dev/null
+polybar --config="~/.config/polybar/config.ini" bar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
