36e6bcc3c025d415f861eb76fad722797bf637c4
Krzysztof Rutkowski <krzysztof.rutk@gmail.com>
2023-12-30 19:15:50 +0100

    Update nvim config

--- config/nvim/lua/evil/plugins/indent-blankline.lua
+++ config/nvim/lua/evil/plugins/indent-blankline.lua
@@ -1,10 +1,7 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
-  -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help indent_blankline.txt`
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-  },
+  main = "ibl",
+  opts = {},
 }
